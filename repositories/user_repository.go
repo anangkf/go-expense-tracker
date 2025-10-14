9eb5d46383130ca75392ec8a6a57685024df237f
anangkf <anangkhoirulfadli@gmail.com>
2025-10-14 23:36:20 +0700

    feat: enhance user registration to include default categories and update User model to support categories

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -14,8 +14,8 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-func (r *UserRepository) Create(user models.User) error {
-	return r.db.Create(&user).Error
+func (r *UserRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
 }
 
 func (r *UserRepository) GetByEmail(email string) (*models.User, error) {
